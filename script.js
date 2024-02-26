45dc05e63f0228cf028b7f4d64b300849845ee80
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-26 20:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function promise(){
+	setTimeout(()=>{
+ 	 	return new Promise((resolve,reject)=>{
+			resolve(document.getElementById("output").textContent="Hello,world!")
+		})
+  	},1000)
+}
+promise();
